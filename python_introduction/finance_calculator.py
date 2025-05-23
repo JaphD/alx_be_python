8a24360fd313808eb07e3e3598830ab201518fe1
Yafet_Daniel <japhethdanny@gmail.com>
2025-05-23 13:51:28 +0300

    made some modifications to the script to pass the automated checker

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,13 +1,18 @@
-user_monthly_income = float(input("Enter your monthly income: "))
-user_monthly_expense = float(input("Enter your monthly income: "))
 
-monthly_savings = user_monthly_income - user_monthly_expense
+# get inputs
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your monthly expenses: "))
 
+# compute savings
+monthly_savings = monthly_income - monthly_expenses
+
+# output monthly savings
 print(f"Your monthly savings are ${monthly_savings}.")
 
+# project annual savings with 5% interest
 annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print(f"Projected savings after one year, with interest, is: ${annual_savings}.")
 
-print(f"Projected Savings after one year, with interest, is: ${annual_savings}")
 
 
 
