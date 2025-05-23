96119b764e4423b8173a321cd90c535f2ca4b678
Yafet_Daniel <japhethdanny@gmail.com>
2025-05-23 13:45:33 +0300

    Created a python script that can estimate annual savings

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,13 @@
+user_monthly_income = int(input("Enter your monthly income: "))
+user_monthly_expense = int(input("Enter your monthly income: "))
+
+monthly_savings = user_monthly_income - user_monthly_expense
+
+print(f"Your monthly savings are ${monthly_savings}.")
+
+annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+print(f"Projected Savings after one year, with interest, is: ${annual_savings}")
+
+
+
