f31e48df99808bcfc367ff041292bd4307f7e040
Yafet_Daniel <japhethdanny@gmail.com>
2025-06-06 22:35:09 +0300

    Made some modifications to pass the automated checker

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     if operation == "add":
         return num1 + num2
     elif operation == "subtract":
@@ -12,3 +12,4 @@ def perform_operation(num1: float, num2: float, operation: str):
             return num1 / num2
     else:
         return "You didn't input a correct operation"
+
