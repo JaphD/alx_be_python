4882bf103db84887b16969b27dfacc5060ee10d6
Yafet Daniel Haileselassie <japhethdanny@gmail.com>
2025-06-08 08:27:59 +0300

    Created explore_datetime.py to display current datetime and calculate future date using user input

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,25 @@
+import datetime
+
+current_date = datetime.datetime.now()
+
+def display_current_datetime():
+    global current_date
+
+    print(f"Current date and time: {current_date.strftime("%Y-%m-%d")} {current_date.strftime("%H:%M:%S")}")
+
+
+def calculate_future_date():
+    global current_date
+    global days
+
+    extra_days = datetime.timedelta(days)
+
+    future_date = current_date + extra_days
+
+    print(f"Future date: {future_date.strftime("%Y-%m-%d")}")
+
+
+display_current_datetime()
+days = int(input("Enter the number of days to add to the current date: "))
+calculate_future_date()
+
